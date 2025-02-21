44de55dbdac76d3f12cf9e80e88c37e52b751e9d
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 20:28:54 +0000

    Add ecosystems styles and helper methods; implement shared assets update task

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def meta_title
+    [@meta_title, 'Ecosyste.ms: Diff'].compact.join(' | ')
+  end
+
+  def meta_description
+    @meta_description || app_description
+  end
+
+  def app_name
+    "Diff"
+  end
+
+  def app_description
+    "An open API service to generate diffs between package releases for many open source software ecosystems."
+  end
 end
