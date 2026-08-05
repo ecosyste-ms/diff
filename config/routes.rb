e744c68b73597becb2a6fdfdb5da2a2c388663b3
Andrew Nesbitt <andrewnez@gmail.com>
2026-08-05 13:09:26 +0100

    Mark sidekiq session cookie secure in production
    httponly and same_site: :lax are already the Rails defaults; only secure
    is not, because force_ssl is off. Traffic is Cloudflare-fronted HTTPS so
    exposure is minimal, but there is no reason not to set it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Sidekiq::Web.use ActionDispatch::Cookies
-Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_diff_sidekiq_session', path: '/sidekiq'
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_diff_sidekiq_session', path: '/sidekiq', secure: Rails.env.production?
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
